aae4ddf2acaaa05c56caca4a01da6f08b18385ff
tjquillan <tjquillan@gmail.com>
2022-03-22 09:35:26 -0400

    Add tech for route registration

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -3,6 +3,7 @@ import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
+import { regiserRoutes } from "./routes";
 
 const app = express();
 
@@ -10,5 +11,7 @@ app.use(compression());
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
+
+regiserRoutes(app);
 
 export default app;
---
+++ apps/backend/src/routes/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Sets up routes on the given express app
+ *
+ * @param app The express app to register routes on
+ */
+export function regiserRoutes(app: Express.Application): void {
+  //TODO: Add route registration when there are routes
+}
