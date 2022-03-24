05c544f581c8c8fb48f16afceaf89b61e1e1ada2
Kode <kodewhitworth01@gmail.com>
2022-03-24 16:35:38 -0400

    Added unit test to create the habit endpoint and cleaned up the habit.ts file

--- apps/backend/src/util/dateFormating.ts
+++ apps/backend/src/util/dateFormating.ts
@@ -1,9 +1,10 @@
 import { Habit } from "@prisma/client";
 
 /**
- * Function that will cast the stringified DateTime attributes
- * of a given list of habits to timestamps
- * @param habits
+ * Cast the stringified DateTime attributes of
+ * a given list of habits to timestamps
+ * @param habits List of habits to be formated
+ * @returns List of correctly formated habits
  */
 export function formatHabits(habits: Habit[]) {
   return habits.map((habit) => ({
@@ -11,4 +12,18 @@ export function formatHabits(habits: Habit[]) {
     dateCreated: habit.dateCreated.getTime(),
     endDate: habit.endDate.getTime(),
   }));
+}
+
+/**
+ * Cast the stringified DataTime attributes of a given
+ * habit to timestamps
+ * @param habit - Habit to be formated
+ * @returns Correctly formated habit
+ */
+export function formatHabit(habit: Habit) {
+  return {
+    ...habit,
+    dateCreated: habit.dateCreated.getTime(),
+    endDate: habit.endDate.getTime(),
+  };
 }
--- apps/backend/test/habit.test.ts
+++ apps/backend/test/habit.test.ts
@@ -1,32 +1,46 @@
 import request from "supertest";
 import prismaMock from "./prisma-mock";
 import app from "../src/app";
+import { response } from "express";
 
 describe("/habits", () => {
+  const habit = {
+    id: 1,
+    name: "jog",
+    dateCreated: new Date(Date.now()),
+    endDate: new Date(Date.now()),
+    frequency: "4",
+    creator: "Bob",
+    creatorId: 12345,
+  };
+
+  // Format habit correctly
+  const habitRes = {
+    ...habit,
+    dateCreated: habit.dateCreated.getTime(),
+    endDate: habit.endDate.getTime(),
+  };
+
   describe("GET", () => {
-    const habit = {
-      id: 1,
-      name: "jog",
-      dateCreated: new Date(Date.now()),
-      endDate: new Date(Date.now()),
-      frequency: "4",
-      creator: "Bob",
-      creatorId: 12345,
-    };
-
-    const habitRes = {
-      ...habit,
-      dateCreated: habit.dateCreated.getTime(),
-      endDate: habit.endDate.getTime(),
-    };
-
-    test("standard request", () => {
+    test("View a single habit", () => {
       prismaMock.habit.findMany.mockResolvedValue([habit]);
       return request(app)
         .get("/habits/1")
         .then((response) => {
           expect(response.statusCode).toBe(200);
           expect(response.body).toStrictEqual([habitRes]);
+        });
+    });
+  });
+
+  describe("POST", () => {
+    test("Create a single habit", () => {
+      prismaMock.habit.create.mockResolvedValue(habit);
+      return request(app)
+        .post("/habits/")
+        .then((response) => {
+          expect(response.statusCode).toBe(200);
+          expect(response.body).toStrictEqual(habitRes);
         });
     });
   });
