72f497cbb1f01fb126ffa942e6e219d431903fef
tjzetty <tjzetty@mtu.edu>
2022-03-24 15:56:05 -0400

    Added Create and Delete Habit UI

--- apps/frontend/src/pages/Home.tsx
+++ apps/frontend/src/pages/Home.tsx
@@ -5,9 +5,47 @@ import {
   CardActions,
   CardContent,
   Typography,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  DialogContentText,
+  TextField,
+  CardActionArea,
+  CardMedia,
 } from "@mui/material";
+import * as React from "react";
 
 export default function Home() {
+  const [openDelete, setOpenDelete] = React.useState(false);
+  const [openCreate, setOpenCreate] = React.useState(false);
+
+  const handleClickOpenDelete = () => {
+    setOpenDelete(true);
+  };
+
+  const handleCloseDelete = () => {
+    setOpenDelete(false);
+  };
+
+  const handleClickOpenCreate = () => {
+    setOpenCreate(true);
+  };
+
+  const handleCloseCreate = () => {
+    setOpenCreate(false);
+  };
+
+  // TODO: Don't console log information in production!!!
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      habitName: data.get("habit-name"),
+      habitDesc: data.get("habit-description"),
+    });
+  };
+
   return (
     <Box
       component="main"
@@ -20,6 +58,25 @@ export default function Home() {
         flexWrap: "wrap",
       }}
     >
+      <Card
+        sx={{ maxWidth: 275, margin: 2, maxHeight: 130 }}
+        onClick={handleClickOpenCreate}
+      >
+        <CardActionArea>
+          <CardContent>
+            <Typography variant="h5" component="div">
+              New Habit
+            </Typography>
+          </CardContent>
+          <CardMedia
+            component="img"
+            height="65"
+            image="https://lh3.googleusercontent.com/WyK_38n4GGPtqitT0IP7dVq8n9edWFdL__DAJfaxMNEmqtdyJv9WKfAEjuArMO8lt6WOyzwRYCO-xKId8CBW6qr4O3JdczTIo4v3FOH7c-q_VZXDbGM=w1064-v0"
+            alt="create-habit"
+          />
+        </CardActionArea>
+      </Card>
+
       <Card sx={{ minWidth: 275, margin: 2 }}>
         <CardContent>
           <Typography variant="h5" component="div">
@@ -30,7 +87,7 @@ export default function Home() {
         <CardActions>
           <Button size="small">Update</Button>
           <Button size="small">Progress</Button>
-          <Button size="small" color="error">
+          <Button size="small" color="error" onClick={handleClickOpenDelete}>
             Delete
           </Button>
         </CardActions>
@@ -46,7 +103,7 @@ export default function Home() {
         <CardActions>
           <Button size="small">Update</Button>
           <Button size="small">Progress</Button>
-          <Button size="small" color="error">
+          <Button size="small" color="error" onClick={handleClickOpenDelete}>
             Delete
           </Button>
         </CardActions>
@@ -62,7 +119,7 @@ export default function Home() {
         <CardActions>
           <Button size="small">Update</Button>
           <Button size="small">Progress</Button>
-          <Button size="small" color="error">
+          <Button size="small" color="error" onClick={handleClickOpenDelete}>
             Delete
           </Button>
         </CardActions>
@@ -78,7 +135,7 @@ export default function Home() {
         <CardActions>
           <Button size="small">Update</Button>
           <Button size="small">Progress</Button>
-          <Button size="small" color="error">
+          <Button size="small" color="error" onClick={handleClickOpenDelete}>
             Delete
           </Button>
         </CardActions>
@@ -94,11 +151,67 @@ export default function Home() {
         <CardActions>
           <Button size="small">Update</Button>
           <Button size="small">Progress</Button>
-          <Button size="small" color="error">
+          <Button size="small" color="error" onClick={handleClickOpenDelete}>
             Delete
           </Button>
         </CardActions>
       </Card>
+
+      <Dialog
+        open={openDelete}
+        onClose={handleCloseDelete}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Are you sure you want to delete this habit?"}
+        </DialogTitle>
+        <DialogActions>
+          <Button onClick={handleCloseDelete}>Keep</Button>
+          <Button color="error" onClick={handleCloseDelete} autoFocus>
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      <Dialog open={openCreate} onClose={handleCloseCreate}>
+        <Box component="form" onSubmit={handleSubmit} noValidate>
+          <DialogTitle>Create a new habit</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Enter a name and description for the habit:
+            </DialogContentText>
+            <TextField
+              autoFocus
+              required
+              margin="dense"
+              id="habit-name"
+              name="habit-name"
+              label="Habit Name"
+              type="name"
+              fullWidth
+              variant="standard"
+            />
+            <TextField
+              margin="dense"
+              id="habit-description"
+              name="habit-description"
+              label="Habit Description"
+              type="description"
+              fullWidth
+              variant="standard"
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleCloseCreate} color="error">
+              Cancel
+            </Button>
+            <Button onClick={handleCloseCreate} type="submit">
+              Create
+            </Button>
+          </DialogActions>
+        </Box>
+      </Dialog>
     </Box>
   );
 }
