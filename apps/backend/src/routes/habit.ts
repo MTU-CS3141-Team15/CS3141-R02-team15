432fe4de12a4e75a33ff7473b160bddfbe5be99e
Chanpech <70418825+Chanpech@users.noreply.github.com>
2022-04-05 22:37:03 -0400

    Added the router.delete for habit
    I added router delete for habit. I added a lot of comments to make sense of everything while working with it. I also looked into delete many habits but implementation will need to be postponed until select features are added in

--- apps/backend/src/routes/habit.ts
+++ apps/backend/src/routes/habit.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { userInfo } from "os";
 import requireAuth from "../auth/middleware";
 import prisma from "../db";
 import asyncHandler from "../util/asyncHandler";
@@ -113,4 +114,51 @@ router.get(
   })
 );
 
+// Delete a habit
+router.delete(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    //First identify the id
+    const habitId = parseInt(req.params.id); //This should paras the string id to int.
+
+    /* This query should deletes a specific habit and
+         it uses select to return the name and end date of the deleted habit.
+         Not sure if this is userful but I'll keep it in for now.*/
+    const habit = await prisma.habit.delete({
+      where: {
+        id: habitId,
+      },
+      select: {
+        name: true,
+        endDate: true,
+      },
+    });
+
+    if (habit) {
+      res.send(habit); //res here should return the select
+    } else {
+      res.status(400).send(); //res return error
+    }
+  })
+);
+
+/**
+// Delete many habits - Notes: will need further development after we implement select features
+router.delete('/',
+      asyncHandler(async(req, res) =>{
+        const habitId = parseInt(req.params.id); //Find id
+
+        /** 
+         * Tihs functions will first have to find all the selected habits and their ids.
+         * Then it will then delete all of the habits that contain that id.
+         * For now I am thinking it might have something to do with findMany() and deleteMany().
+        
+        const habit = await prisma.habit.deleteMany({
+          where:{
+            id: habitId,
+          }
+        });
+      })
+);
+*/
 export default router;
